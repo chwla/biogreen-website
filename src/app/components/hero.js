fc5b9285a7135af5e2aa5aaeb22be04f258ad25d
chwla <chwla75@gmail.com>
2025-09-30 18:09:43 +0530

    updated hero image

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="relative w-full" style={{ height: 'calc(100dvh - var(--header-height, 5rem))' }}>
       <Image 
-        src="/photos/hero.jpeg" 
+        src="/photos/updated_hero.jpeg" 
         alt="BioGreen sustainable fertilizer hero banner"
         fill
         className="object-cover"
